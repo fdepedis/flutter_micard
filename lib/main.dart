32eb5884f8c5f80d0c5ba4d881cf39d14de48012
Flavio <flavio.depedis@gmail.com>
2020-10-26 18:21:30 +0100

    Lesson 46

--- lib/main.dart
+++ lib/main.dart
@@ -45,13 +45,41 @@ class _MyHomePageState extends State<MyHomePage> {
       ),*/
       backgroundColor: Colors.teal,
       body: SafeArea(
-        child: Container(
-          height: 100.0,
-          width: 100.0,
-          margin: EdgeInsets.only(left: 30.0),
-          padding: EdgeInsets.all(20.0),
-          color: Colors.white,
-          child: Text('Hello'),
+        child: Row(
+          //mainAxisSize: MainAxisSize.min,
+          //verticalDirection: VerticalDirection.up,
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: 100.0,
+              //height: 100.0,
+              //margin: EdgeInsets.only(left: 30.0),
+              //padding: EdgeInsets.all(20.0),
+              color: Colors.white,
+              child: Text('Container'),
+            ),
+            SizedBox(
+              //height: 20.0,
+              width: 20.0,
+            ),
+            Container(
+              //width: 300.0,
+              height: 100.0,
+              color: Colors.blue,
+              child: Text('Container'),
+            ),
+            Container(
+              //width: 100.0,
+              height: 100.0,
+              color: Colors.red,
+              child: Text('Container'),
+            ),
+            /*Container(
+              width: double.infinity,
+              height: 10.0,
+            ),// infinity container*/
+          ],
         ),
       ),
       /*body: Center(
