e232bf7496736c89e10523ab45f0bc5e898117d5
Flavio <flavio.depedis@gmail.com>
2020-10-26 20:30:10 +0100

    Lesson 47

--- lib/main.dart
+++ lib/main.dart
@@ -48,32 +48,47 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Row(
           //mainAxisSize: MainAxisSize.min,
           //verticalDirection: VerticalDirection.up,
-          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
               width: 100.0,
               //height: 100.0,
               //margin: EdgeInsets.only(left: 30.0),
               //padding: EdgeInsets.all(20.0),
-              color: Colors.white,
-              child: Text('Container'),
+              color: Colors.red,
+              //child: Text('Container'),
+            ),
+            SizedBox(
+              //height: 20.0,
+              width: 20.0,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.yellow,
+                  //child: Text('Container'),
+                ),
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.green,
+                  //child: Text('Container'),
+                ),
+              ],
             ),
             SizedBox(
               //height: 20.0,
               width: 20.0,
             ),
             Container(
-              //width: 300.0,
-              height: 100.0,
+              width: 100.0,
+              //height: 100.0,
               color: Colors.blue,
-              child: Text('Container'),
-            ),
-            Container(
-              //width: 100.0,
-              height: 100.0,
-              color: Colors.red,
-              child: Text('Container'),
+              //child: Text('Container'),
             ),
             /*Container(
               width: double.infinity,
@@ -98,7 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),*/
-      /* floatingActionButton: FloatingActionButton(
+      /*floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
